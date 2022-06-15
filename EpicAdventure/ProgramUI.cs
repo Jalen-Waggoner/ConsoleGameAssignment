fd607383ba2b1ed14bab796da7ce8bd7f2a5b83e
Jalen Waggoner <jalenwaggoner@gmail.com>
2022-06-15 12:05:00 -0400

    Added death scenario, and random encounters. Fixed player choice at start of game.

--- EpicAdventure/ProgramUI.cs
+++ EpicAdventure/ProgramUI.cs
@@ -9,18 +9,28 @@ public class ProgramUI {
     static Random rand;
     public void Run(){
         rand = new Random(DateTime.Now.Millisecond);
+        bool continueToRun = true;
         RunMenu();
         
         Encounters.Boss1();
+        if (ProgramUI.currentPlayer.health <= 0){
+            continueToRun = false;
+        }
+        while(continueToRun){
+        Encounters.RandomEncounter();
         Encounters.Boss2();
+
         Encounters.Boss3();
+
         Encounters.Boss4();
-        Encounters.FinalBoss();
+
+        Encounters.FinalBoss();}
         // Other Encounters
     }
 
     public void RunMenu() {
-    
+    bool selectionNeeded = true;
+
     Console.Clear();
     Console.WriteLine("---------------Welcome to DragonBall TJ---------------\n" +
     "Press any key to embark on this tale for the ages!");
@@ -34,7 +44,8 @@ public class ProgramUI {
     Console.ReadKey();
     Console.WriteLine("\"Odd. Could that be? No surely it couldn't. Unless?\"");
     Console.ReadKey();
-    
+
+    while(selectionNeeded) {
     Console.WriteLine("Do you go to inspect the anomaly or lay back down for another nap?\n" +
     "1. Charge!\n" +
     "2. Zzzzzz");
@@ -43,6 +54,7 @@ public class ProgramUI {
     
     Console.WriteLine("Uncertain yet determined, you tighten your gi, strap a Power Pole to your side," + 
     "and set your dreads in place before speeding down the hill towards your quarry.");
+    selectionNeeded = false;
     } 
     else if (selection == "2") {
         Console.WriteLine("You fall flat on your back, stretch out your arms and then weave your fingers behind your head. Slowly drifting back to sleep wearing a grin from ear to ear.");
@@ -52,9 +64,13 @@ public class ProgramUI {
         Console.WriteLine("All of a sudden, you hear a CRASH coming from the direction of the anomaly!");
         Console.ReadKey();
         Console.WriteLine("Frustrated, you exclaim \"I can't sleep with all that ruckus!\" and you reluctantly get up with a yawn to investigate further.");
+        selectionNeeded = false;
     } 
     else {
         Console.WriteLine("Please input a valid option.");
+        Console.ReadKey();
+        Console.Clear();
+    }
     }
     Console.ReadKey();
     Console.Clear();
@@ -63,7 +79,7 @@ public class ProgramUI {
     public class TJ {
         public string name = "Saiyan TJ";
         public int coins = 0;
-        public int health = 9000;
+        public int health = 1;
         public int damage = 1;
         public int armorValue = 0;
         public int potion = 5;
@@ -73,18 +89,18 @@ public class ProgramUI {
         //Needs 7 Dragon Balls to end humanity
     }
 
-// private void WaitForKey() {
-//     public bool Prompt;
-//     Prompt prompt = new Prompt;
-//     Console.ReadKey();
-//     if (prompt = true){
-//     Console.WriteLine("Press any key to continue...");
-//     }
-// }
     public class Encounters
     {
         
-
+        public static void RandomEncounter() {
+            System.Console.WriteLine("While exploring you some rustling and glance behind you to see a flick of movement.");
+            Console.ReadKey();
+            System.Console.WriteLine("\"Come on out so that we can be done with this! I have Dragon Balls to collect!\" you taught in the direction of the noise.");
+            Console.ReadKey();
+            System.Console.WriteLine($"After a moment of deliberation, figure emerges from the shadows and charges for you head on!");
+            Console.ReadKey();
+            Combat( true, "", 0, 0);
+        }
         
         //Encounters
         public static void Boss1() {
@@ -93,7 +109,7 @@ public class ProgramUI {
             Console.ReadKey();
             Console.WriteLine($"You square your jaw and set your gaze directly at Android 13. Daring him to take you on!");
             Console.ReadKey();
-            Combat("Android 13", 1, 4);
+            Combat(false,"Android 13", 1, 4);
             Console.WriteLine($"\"AGHAHHHH! How could this be!? Thwarted by a grubby little peon like you!?\" Android 13 yells in defeat as you brush the dust off your sleeve.");
             Console.WriteLine("As you look down your nose at the crumpled mess of a monster below you, you can't help but feel your ego swell to even greater heights.");
             Console.Clear();
@@ -105,26 +121,25 @@ public class ProgramUI {
             System.Console.WriteLine("You quickly realize its another Android heading your way");
             Console.ReadKey();
             System.Console.WriteLine($"You swoop to the left, dodging his attack. \"Are we really going to do this?\" you ask, obviously annoyed.");
-            System.Console.WriteLine($"As he's gathering his balance he lets out a gutteral scream \"Fight me and PERISH!\"");
+            System.Console.WriteLine($"As he's gathering his balance he lets out a guttural scream \"Fight me and PERISH!\"");
             Console.ReadKey();
-            Combat("Android 15", 3, 8); 
-            System.Console.WriteLine("Android 15 sputters unitelligibly, reaching his hand out, grasping the empty air around him as if it was his last chance of survival.");
+            Combat(false, "Android 15", 3, 8);
+            System.Console.WriteLine("Android 15 sputters unintelligibly, reaching his hand out, grasping the empty air around him as if it was his last chance of survival.");
             System.Console.WriteLine($"\"You should've known you'd be no match for me, Android.\" you snide, sauntering away zealously.");
-
         }
         public static void Boss3() {
             // Encounter description Console.WriteLine();
             // Console.ReadKey();
             // Console.WriteLine($"You square your jaw and set your gaze directly at Android 13. Daring him to take you on!");
             // Console.ReadKey();
-            Combat("Android 17", 9, 16); //name, attack value, health.
+            Combat(false, "Android 17", 9, 16); //name, attack value, health.
         }
         public static void Boss4() {
             // Encounter description Console.WriteLine();
             // Console.ReadKey();
             // Console.WriteLine($"You square your jaw and set your gaze directly at Android 13. Daring him to take you on!");
             // Console.ReadKey();
-            Combat("Android 21", 27, 32); //name, attack value, health.
+            Combat(false, "Android 21", 27, 32); //name, attack value, health.
         }
         
         
@@ -133,7 +148,7 @@ public class ProgramUI {
             // Console.ReadKey();
             // Console.WriteLine($"You square your jaw and set your gaze directly at Android 13. Daring him to take you on!");
             // Console.ReadKey();
-            Combat("Vegeta", 135, 128); //name, attack value, health.
+            Combat(false, "Vegeta", 135, 128); //name, attack value, health.
             System.Console.WriteLine("The boss being defeated you glance to the sky and see a majestic dragon.");
             System.Console.WriteLine("\"Greetings TJ! You have collected all of my ballz. As your reward for handling them with care and honor, I bestow but one wish. An all powerful wish that can grant you anything you desire.\"");
             System.Console.WriteLine("\"Now then, what shall it be?!\"");
@@ -148,15 +163,20 @@ public class ProgramUI {
         }
         
         //Encounter Tools
-        public static void Combat(string name, int power, int health) {
+        public static void Combat( bool random, string name, int power, int health) {
             string n = "";
             int p = 0;
             int h = 0;
-        
+            if (random){
+                n = GetName();
+                p = rand.Next(1, 10);
+                h = rand.Next(1, 10);
+            }
+            else{
             n = name;
             p = power;
             h = health;
-            
+            }
             while (h > 0 ) {
                 Console.Clear();
                 Console.WriteLine(n);
@@ -179,7 +199,7 @@ public class ProgramUI {
                     damage = 0;} 
                     int attack = rand.Next(0, ProgramUI.currentPlayer.weaponValue) + rand.Next(1,4);
                     
-                    Console.WriteLine($"You swipe horizontally with your Power Pole! Delivering a staggering blow that deals {damage}");
+                    Console.WriteLine($"You swipe horizontally with your Power Pole! Delivering a staggering blow that deals {attack}");
                     Console.WriteLine($"{n} follows up with their own attack!    You lose {damage} health");
                     ProgramUI.currentPlayer.health -= damage;
                     h -= attack;
@@ -235,14 +255,32 @@ public class ProgramUI {
                         ProgramUI.currentPlayer.health -= damage;
                 }
                 Console.ReadKey();
-                }
-                if (ProgramUI.currentPlayer.health <= 0)
-                {
-                    //Death code
-                    System.Console.WriteLine($"Dealing the final blow, {n} cackles violently. \"You have been DEFEATED!\"");
-                    Console.ReadKey();
-                }
+
+            if (ProgramUI.currentPlayer.health <= 0){
+                Console.Clear();
+                System.Console.WriteLine("You slump to your knees, defeated, waiting for the final blow to strike.");
+                Console.ReadKey();
+                System.Console.WriteLine("You think to yourself,\"Maybe I should just think of this as one long, long nap.\" Everything fades to black.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
             }
         }
-    }
-    
+            int c = rand.Next(10,50);
+            public static string GetName(){
+                switch(rand.Next(0,4)){
+                    case 0:
+                        return "Giant Bear";
+                    case 1: 
+                        return "Man-eating Venus Flytrap";
+                    case 2:
+                        return "Todd";
+                    case 3: 
+                        return "Hired Mercenary";
+                    case 4: 
+                        return "Yeti";
+                }
+            return "Ruffian";
+            }
+            }
+        }
